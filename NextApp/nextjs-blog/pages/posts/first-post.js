b3786e67fbdf963638cb3b1b35600bbf9148fa14
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-03-29 17:33:57 +0300

    Image, Head, Script

--- NextApp/nextjs-blog/pages/posts/first-post.js
+++ NextApp/nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,25 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
+
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );
-}
+    return (
+      <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">← Back to home</Link>
+        </h2>
+      </>
+    );
+  }
